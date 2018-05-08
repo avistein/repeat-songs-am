38e7239e3d30c9d8a554174087dfa62834eff451
avistein <aviksarkar15@gmail.com>
2018-05-08 21:18:48 +0530

    Fixed minor bugs

--- repeatsongs.js
+++ repeatsongs.js
@@ -1,8 +1,12 @@
+var clicked = false;
 function timeCount(){
 	var timeRemaining = document.getElementsByClassName("timeRemaining")[0].innerHTML;
 	var repeatAll = document.getElementsByClassName("repeatButton")[0].getAttribute("aria-checked");
-		if(timeRemaining == "0:01" && repeatAll == "false"){
+	if(timeRemaining == "0:01" && repeatAll == "false" && !clicked){
 		document.getElementsByClassName("previousButton")[0].click();
+		clicked = true;
 	}
+	else if(timeRemaining != "0:01")
+		clicked = false;
 }
 setInterval(timeCount,1000);
